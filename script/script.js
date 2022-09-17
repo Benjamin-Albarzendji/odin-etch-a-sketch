dd0a39d35eb57e4f283245b629f0f5cbd79f5d36
Benjamin-Albarzendji <benjamin.albarzendji@gmail.com>
2022-09-17 22:25:25 +0200

    Fixed background color, leaving painted untouched

--- script/script.js
+++ script/script.js
@@ -1,7 +1,6 @@
 //Global variables for color choices
 let penColor = "#000000";
-let bgColor = "#FFFFFFF"
-
+let bgColor = "#FFFFFFF";
 
 start();
 
@@ -11,54 +10,53 @@ function start() {
   createGrid();
 
   //Event listener for the slider
-  gridValue = document.getElementById("slider");
+  let gridValue = document.getElementById("slider");
   gridValue.oninput = function () {
     //Updates the value of the grid slider
-    sliderText = document.getElementById("sliderValue");
+    let sliderText = document.getElementById("sliderValue");
     sliderText.textContent = document.getElementById("slider").value;
 
     //Calls a new grid)
     newGrid();
   };
 
-      //Listener for penColor
-      penColorChange = document.getElementById("brusch")
-      penColorChange.onchange = function (){
-        penColor = penColorChange.value
-      }
+  //Listener for penColor
+  let penColorChange = document.getElementById("brusch");
+  penColorChange.onchange = function () {
+    penColor = penColorChange.value;
+  };
 
-      //Listener for backgroundColor
-      bgColorChange = document.getElementById("background")
-      bgColorChange.onchange = function (){
-        bgColor = bgColorChange.value
-        newGrid()
-      }
+  //Listener for backgroundColor
+  let bgColorChange = document.getElementById("background");
+  bgColorChange.onchange = function () {
+    bgColor = bgColorChange.value;
+    newBackground();
+  };
 
-      //Listener for Clear Button
-      clearBtn = document.getElementById("clear")
-      clearBtn.onclick = function (){
-    newGrid()
-        
-    }
+  //Listener for Clear Button
+  let clearBtn = document.getElementById("clear");
+  clearBtn.onclick = function () {
+    newGrid();
+  };
 }
 
 //The grid function
 function createGrid(gridX = 16, gridY = 16) {
   //Grabs the gridContainer
-  gridContainer = document.querySelector(".gridContainer");
+  let gridContainer = document.querySelector(".gridContainer");
 
   //Creates the rows of the grid container
   for (let i = 0; i < gridY; i++) {
-    gridObject = document.createElement("div");
+    let gridObject = document.createElement("div");
     gridObject.className = "gridObject";
     gridContainer.appendChild(gridObject);
   }
 
   //Loops over the gridrows and adds cells
-  list = document.querySelectorAll(".gridObject");
+  let list = document.querySelectorAll(".gridObject");
   list.forEach((row) => {
     for (let j = 0; j < gridX; j++) {
-      gridCell = document.createElement("div");
+      let gridCell = document.createElement("div");
       gridCell.className = "gridCell";
       gridCell.style.backgroundColor = bgColor;
       row.appendChild(gridCell);
@@ -66,7 +64,7 @@ function createGrid(gridX = 16, gridY = 16) {
   });
 
   //Adds event listeners to all cells
-  cellListener = document.querySelectorAll(".gridCell");
+  let cellListener = document.querySelectorAll(".gridCell");
 
   cellListener.forEach((cell) => {
     //Listening on mousemove and click
@@ -85,19 +83,31 @@ function createGrid(gridX = 16, gridY = 16) {
 
 function newGrid(e) {
   //Removes the old grid
-  oldGrid = document.querySelectorAll(".gridObject");
+  let oldGrid = document.querySelectorAll(".gridObject");
   oldGrid.forEach((cell) => {
     cell.parentNode.removeChild(cell);
   });
 
   //Grabs the valeu from the slider
-  x = gridValue = document.getElementById("slider").value;
-  y = gridValue = document.getElementById("slider").value;
+  let x = (gridValue = document.getElementById("slider").value);
+  let y = (gridValue = document.getElementById("slider").value);
 
   //creates a new grid by calling the function
   createGrid(x, y);
 }
 
 function paint(e) {
-  e.target.style.backgroundColor = penColor
+  e.target.style.backgroundColor = penColor;
+  e.target.id = "painted";
+}
+
+//Adds a new backround while painting
+function newBackground() {
+  //Changed color of only unpanted cells
+  let unpaintedCells = document.querySelectorAll(".gridCell");
+  unpaintedCells.forEach((cell) => {
+    if (cell.id !== "painted") {
+      cell.style.backgroundColor = bgColor;
+    }
+  });
 }
